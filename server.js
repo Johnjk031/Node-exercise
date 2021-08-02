0b824bb42b4058214d3cb7a934d30b5a03b306b1
Johnjk031 <john.juhlinkarlsson@gmail.com>
2021-08-02 23:42:26 +0200

    replaced text with HTML

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const fs = require('fs')
 const port = 1010
 
 const server = http.createServer(function(req, res) {
-    res.write('Hello Node World')
-    res.end()
+    res.writeHead(200, {'Content-Type' : 'text/html' })
+    fs.readFile('index.html', function(error, data){
+        if (error) {
+            res.writeHead(400)
+            res.write('Could not find file')
+        } else {
+            res.write(data)
+        }
+      res.end()
+    })
 })
 
 server.listen(port, function(error) {
